4af50b1bd6508ca6515f8daf77a2e2f334f488ea
charishmabatchu <93284916+charishmabatchu@users.noreply.github.com>
2021-10-27 15:33:13 -0400

    Add files via upload

---
+++ Get-ComputerInfo.ps1
@@ -0,0 +1,81 @@
+﻿<#
+    Author: Charishma Batchu
+    Release Date: 10/27/2021
+
+    Name: Get-ComputerInfo.ps1
+    The purpose of this script is to retrieve basic computer info
+
+    Parameters: ComputerName
+    This is mandatory parameter that is needed to provide the name of the computer to retrieve the information
+
+    Example: Run Get-ComputerInfo.ps1 -ComputerName X060-CTXDT85 to retrieve the info
+#>
+
+[CmdletBinding()]
+PARAM(
+    [Parameter(Mandatory=$true)][String] $ComputerName
+)
+
+try
+{
+    # X060-CTXDT85
+    Write-Host "Chceking connection for Computer $ComputerName"
+    $connection = Test-Connection $ComputerName -Count 1 -Quiet
+     Write-Host "Connection established successfully for Computer $ComputerName"
+
+    if($connection -eq "True")
+    {
+        $params = @{'ComputerName'=$ComputerName;
+                    'Class'='CIM_ComputerSystem';
+                    'ErrorAction'='Stop'}
+        Write-Host "Retrieving information for Computer $ComputerName"
+        $cimInfo = Get-CimInstance @params | 
+                                    Select-Object Caption,
+                                                  Description,
+                                                  InstallDate,
+                                                  Name,
+                                                  Status,
+                                                  DNSHostName,
+                                                  Domain,
+                                                  HypervisorPresent,
+                                                  InfraredSupported,
+                                                  Manufacturer,
+                                                  Model,
+                                                  NumberOfLogicalProcessors,
+                                                  NumberOfProcessors,
+                                                  PartOfDomain,
+                                                  SystemSKUNumber,
+                                                  SystemType,
+                                                  UserName,
+                                                  NameFormat,
+                                                  PrimaryOwnerContact,
+                                                  PrimaryOwnerName,
+                                                  Roles,
+                                                  InitialLoadInfo,
+                                                  LastLoadInfo,
+                                                  PowerManagementCapabilities,
+                                                  PowerManagementSupported,
+                                                  PowerState,
+                                                  ResetCapability,
+                                                  AdminPasswordStatus,
+                                                  AutomaticManagedPagefile,
+                                                  AutomaticResetBootOption,
+                                                  AutomaticResetCapability,
+                                                  CurrentTimeZone,
+                                                  DaylightInEffect
+                                              
+
+
+        if($cimInfo.Name -ne '')
+        {
+            $filePath = "$PSScriptRoot\$ComputerName.json"
+            $cimInfo | ConvertTo-json | Out-File $filePath
+            #$computerInfo = Get-ComputerInfo | ConvertTo-json | Out-File "C:\Users\BATCHUC\Desktop\info.json"
+        }
+         Write-Host "Successfully exported $ComputerName information to $filePath"
+    }
+}
+catch{
+    Write-Warning "Failed to retrieve information for computer $ComputerName"
+    Write-Warning "Error Message $_"
+}
---
+++ Get-GitRepos.ps1
@@ -0,0 +1,63 @@
+﻿<#
+    Author: Charishma Batchu
+    Release Date: 10/27/2021
+ 
+    Name: Get-GitRepo.ps1
+    The purpose of this script is get Gitgub repos under an organization
+    
+    Parameters: 
+    Org : This parameter is needed to provide the name of the organization to retrieve the information
+    PAT : This parameter is used to authorize the GitHub
+    
+    Example: Run Get-GitRepo.ps1 -Org X060-CTXDT85 "charishmabatchu" -PAT "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"
+#>
+
+[CmdletBinding()]
+PARAM(
+    [Parameter(Mandatory=$false)][String] $Org = "charishmabatchu",
+    [Parameter(Mandatory=$false)][String] $PAT = "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"
+)
+
+
+try
+{
+    #check if either of parmaters are null or empty
+    if([string]::IsNullOrEmpty($PAT) -or ([string]::IsNullOrEmpty($Org)))
+    {
+        Write-Host "Make sure PAT and Organization Name are provided"
+    }
+    else
+    {
+        #build authorization header based on PAT
+        $gitHeader = @{Authorization=("Basic{0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "",$PAT))))}
+        $gitRepoUri = "https://api.github.com/users/$Org/repos"
+
+        #check if header is null or empty
+        if($gitHeader -ne '' -or $gitHeader -ne 'null')
+        {
+            #GitHub API call to retrieve repos
+            $Repos = Invoke-RestMethod -Uri $gitRepoUri -ContentType "application/vnd.github.v3+json" -Headers $gitHeader -Method GET
+            Write-Host "Total Repos in $Org gitHub " $Repos.Count 
+            if($Repos.Count -gt 0)
+            {
+                $filePath = "$PSScriptRoot\GitRepos.json"
+                $Repos | ConvertTo-json | Out-File $filePath
+                foreach($repo in $Repos)
+                {
+                    if($repo.name -eq 'Assignment')
+                    {
+                        Write-Host "Code Assignment Repo: "$repo.html_url
+                    }
+                }
+            }
+        }
+        else
+        {
+            Write-Host "Invalid PAT Token"
+        }
+    }
+}
+catch{
+    Write-Warning "Failed to retrieve information from GitHub"
+    Write-Warning "Error Message $_"
+}
